cb0cfb3f7374c80ccf8e0860116796a9889bd3ee
Josh Walawender <jmwalawender@gmail.com>
2019-10-27 23:27:19 -1000

    Gain calc

--- keckdata/analysis/ccdcharacteristics.py
+++ keckdata/analysis/ccdcharacteristics.py
@@ -182,3 +182,184 @@ def determine_dark_current(input, master_bias=None, plot=False,
                 plt.show()
 
     return u.Quantity(DC)
+
+
+def determine_gain(input, master_bias=None, read_noise=None, plot=False,
+                   clippingsigma=5, clippingiters=3, trim=0,
+                   aduthreshold=30000):
+    '''
+    Determine gain from a series of flats of varying exposure time.  This
+    assumes a roughly constant illumination.
+    '''
+    log.info('Determining gain')
+    flat_frames = KeckDataList([kd for kd in input.frames
+                  if kd.type() in ['INTFLAT', 'FLAT']])
+    npds = len(flat_frames.frames[0].pixeldata)
+
+    # Get image statistics for each flat file
+    names = ['file', 'exptime', 'exptime_int']
+    dtypes = ['a100', 'f4', 'i4']
+    names.extend( [f"mean{i}" for i in range(npds)] )
+    dtypes.extend( ['f4']*npds )
+    names.extend( [f"median{i}" for i in range(npds)] )
+    dtypes.extend( ['f4']*npds )
+    names.extend( [f"stddev{i}" for i in range(npds)] )
+    dtypes.extend( ['f4']*npds )
+
+    flat_table = Table(names=names, dtype=dtypes)
+    log.info(f'Step 1: Get image statistics for {flat_frames.len} flat frames')
+    log.info(f'Each frame has {npds} extensions')
+    for flat_frame in flat_frames.frames:
+        log.debug(f'Analyzing {flat_frame.filename()}')
+        row = {'file': flat_frame.filename(),
+               'exptime': flat_frame.exptime(),
+               'exptime_int': int(flat_frame.exptime()),
+               }
+        bs_flat = flat_frame.subtract(master_bias)
+        for i in range(npds):
+            ny, nx = bs_flat.pixeldata[i].data.shape
+            mean, med, std = stats.sigma_clipped_stats(
+                       bs_flat.pixeldata[i][trim:ny-trim,trim:nx-trim],
+                       sigma=clippingsigma, iters=clippingiters) * u.adu
+            log.debug(f'  Flat (mean, med, std) = {mean.value:.1f}, '\
+                      f'{med.value:.1f}, {std.value:.2f}')
+            row[f'mean{i}'] = mean.value
+            row[f'median{i}'] = med.value
+            row[f'stddev{i}'] = std.value
+        flat_table.add_row(row)
+
+    bytime = flat_table.group_by('exptime_int')
+    exptimes = sorted(set(flat_table['exptime_int']))
+    ntimes = len(exptimes)
+    filenames = [kd.filename() for kd in flat_frames.frames]
+    npds = len(flat_frames.frames[0].pixeldata)
+    signal = []
+    variance = []
+    signal_times = []
+    log.info(f"Step 2: Analyze image differences at each of {ntimes} exposure times")
+    for exptime in exptimes:
+        exps = bytime.groups[bytime.groups.keys['exptime_int'] == exptime]
+        nexps = len(exps)
+        log.info(f'  Measuring statistics for {nexps} {exptime:d}s flats')
+        for i in np.arange(0,nexps,2):
+            if i+1 < nexps:
+                flat_fileA = exps['file'][i]
+                flat_fileB = exps['file'][i+1]
+                log.debug(f"{flat_fileA} / {flat_fileB}")
+                A = filenames.index(flat_fileA)
+                B = filenames.index(flat_fileB)
+                expA = flat_frames.frames[A]
+                expB = flat_frames.frames[B]
+
+                frame_signal = []
+                frame_variance = []
+                for j in range(npds):
+                    meanA = exps[i][f'mean{j}']
+                    meanB = exps[i+1][f'mean{j}']
+                    ratio = meanA/meanB
+                    log.debug(f'  Forming difference for extension {j} with scaling ratio {ratio:.4f}')
+                    expB_scaled = expB.pixeldata[j].multiply(ratio)
+                    diff = expA.pixeldata[j].subtract(expB_scaled)                
+                    ny, nx = diff.data.shape
+                    mean, med, std = stats.sigma_clipped_stats(
+                                        diff.data[trim:ny-trim,trim:nx-trim],
+                                        sigma=clippingsigma,
+                                        iters=clippingiters) * u.adu
+                    log.debug(f'    Signal Level = {meanA:.2f}')
+                    log.debug(f'    Variance = {std.to(u.adu).value**2/2.:.2f}')
+                    frame_variance.append(std.to(u.adu).value**2/2.)
+                    frame_signal.append(meanA)
+                signal.append(frame_signal)
+                variance.append(frame_variance)
+                signal_times.append(exps[i]['exptime'])
+    signals = np.array(signal)
+    signal_times = np.array(signal_times)
+    variances = np.array(variance)
+
+    gains = []
+    log.info(f'Step 3: Fit gain and linearity for each extension')
+    for j in range(npds):
+        log.info(f"Fitting model to determine gain for extension {j}")
+        signal = signals[:,j]
+        variance = variances[:,j]
+        ## Fit model to variance vs. signal
+        ## var = RN^2 + 1/g S + k^2 S^2
+        mask = np.array(np.array(signal) > aduthreshold)
+        poly = models.Polynomial1D(degree=2, c0=read_noise[j].to(u.adu).value)
+        poly.c0.fixed = True
+        poly.c2.min = 0.0
+        fitter = fitting.LevMarLSQFitter()
+        y = np.array(variance)[~mask]
+        x = np.array(signal)[~mask]
+        gainfits = fitter(poly, x, y)
+        # perr = np.sqrt(np.diag(fitter.fit_info['param_cov']))
+        ksq = gainfits.c2.value
+        # ksqerr = perr[1]
+        # print('  k^2 = {:.2e} +/- {:.2e} e/ADU'.format(ksq, ksqerr))
+        # print('  k^2 = {:.2e} e/ADU'.format(ksq))
+        g = gainfits.c1**-1 * u.electron/u.adu
+        # gerr = gainfits.c1**-2 * perr[0] * u.electron/u.adu
+        log.info(f'  Gain = {g:.3f}')
+        gains.append(g)
+
+        ## Fit Linearity
+        log.debug('  Fitting linear model to find linearity limit')
+        line = models.Linear1D(intercept=0, slope=500)
+        line.intercept.fixed = True
+        fitter = fitting.LinearLSQFitter()
+        x = np.array(signal_times)[~mask]
+        y = np.array(signal)[~mask]
+        linearity_fit = fitter(line, x, y)
+
+        if plot is True:
+            log.info('  Generating figures with flat statistics and gain fits')
+            plt.figure(figsize=(12,12))
+            for i,plottype in enumerate(['', '_log']):
+                plt.subplot(2,1,i+1)
+                ax = plt.gca()
+                x = np.array(signal)[mask]
+                y = np.array(variance)[mask]
+                ax.plot(x, y, 'ko', alpha=0.3, markersize=5, markeredgewidth=0)
+                x = np.array(signal)[~mask]
+                y = np.array(variance)[~mask]
+                if plottype == '_log':
+                    ax.semilogx(x, y, 'ko', alpha=1.0, markersize=8, markeredgewidth=0)
+                    sig_fit = np.linspace(min(signal), max(signal), 50)
+                    var_fit = [gainfits(x) for x in sig_fit]
+                    ax.semilogx(sig_fit, var_fit, 'k-', alpha=0.7,
+                            label='Gain={g.value:.2f}e/ADU')
+                else:
+                    ax.plot(x, y, 'ko', alpha=1.0, markersize=8, markeredgewidth=0)
+                    sig_fit = np.linspace(min(signal), max(signal), 50)
+                    var_fit = [gainfits(x) for x in sig_fit]
+                    ax.plot(sig_fit, var_fit, 'k-', alpha=0.7,
+                            label=f'Gain={g.value:.2f}e/ADU')
+                ax.set_ylabel('Variance')
+                ax.set_xlabel('Mean Level (ADU)')
+                ax.grid()
+                ax.legend(loc='upper left', fontsize=10)
+            plot_file = Path(f'Gain_{j}.png')
+            plt.savefig(plot_file, bbox_inches='tight', pad_inches=0.10)
+
+            log.info('  Generating figure with linearity plot')
+            plt.figure(figsize=(12,12))
+            for i,plottype in enumerate(['', '_log']):
+                ax = plt.gca()
+                time = np.array(signal_times)
+                counts = np.array(signal)
+                fit_counts = [linearity_fit(t) for t in time]
+                y = (counts-fit_counts)/counts * 100.
+                if plottype == '_log':
+                    ax.semilogx(counts, y, 'ko', alpha=0.5, markersize=5, markeredgewidth=0)
+                    ax.semilogx([min(counts), max(counts)], [0, 0], 'k-')
+                else:
+                    ax.plot(counts, y, 'ko', alpha=0.5, markersize=5, markeredgewidth=0)
+                    ax.plot([min(counts), max(counts)], [0, 0], 'k-')
+                ax.set_xlabel('Mean Level (ADU)')
+                ax.set_ylabel('Signal Decrement (%) [(counts-fit)/counts]')
+            #     plt.ylim(np.floor(min(decrements)), np.ceil(max(decrements)))
+                ax.grid()
+            plot_file = Path(f'Linearity_{j}.png')
+            plt.savefig(plot_file, bbox_inches='tight', pad_inches=0.10)
+    return u.Quantity(gains)
+
