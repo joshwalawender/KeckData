25be6b9d874588d458abebe1bfe73f984d81136c
Josh Walawender <jmwalawender@gmail.com>
2019-10-28 09:41:36 -1000

    Handle imaging mode

--- keckdata/infrared.py
+++ keckdata/infrared.py
@@ -14,11 +14,11 @@ class MOSFIREData(KeckData):
     def verify(self):
         """Verifies that the data which was read in matches an expected pattern
         """
-        if len(self.headers) != 5:
+        if len(self.headers) not in [1, 5]:
             raise IncorrectNumberOfExtensions("header", "5", self)
         if len(self.pixeldata) not in [1, 2, 3]:
             raise IncorrectNumberOfExtensions("pixel", "1, 2, or 3", self)
-        if len(self.tabledata) != 4:
+        if len(self.tabledata) not in [0,4]:
             raise IncorrectNumberOfExtensions("table", "4", self)
 
     def type(self):
